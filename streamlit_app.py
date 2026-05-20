aecce416a09ef747ac743e0c1dbdbfdb127df5e1
Sanika Thete <sanikathete28@gmail.com>
2026-05-20 10:43:09 +0530

    Correct fruit variable name and modify API URL
    Fix variable name typo and update URL construction.

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,10 @@ if ingredients_list :
 
     for fruit_choosen in ingredients_list:
         ingredients_string+=fruit_choosen + ''
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_choosen + 'Nutrition information')
         url = ("https://my.smoothiefroot.com/api/fruit/watermelon".strip()+fruit_choosen)
         response = requests.get(url)
