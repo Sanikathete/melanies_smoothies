83798c4eb98ecc66d032b22de8ca20f0e2dbda2f
Sanika Thete <sanikathete28@gmail.com>
2026-05-20 10:57:55 +0530

    Fix URL construction for fruit nutrition API

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if ingredients_list :
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_choosen + 'Nutrition information')
-        url = ("https://my.smoothiefroot.com/api/fruit/watermelon".strip()+fruit_choosen)
+        url = (f"https://my.smoothiefroot.com/api/fruit/,{search_on}".strip()+fruit_choosen)
         response = requests.get(url)
         sf_df=st.dataframe(data=response.json(),use_container_width=True)
 
